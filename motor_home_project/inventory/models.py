057077758890ce7932475bd3aeb517388a1593d9
Alexander Rubin <your@email.here>
2025-03-24 16:19:33 -0500

    build sales models

--- motor_home_project/inventory/models.py
+++ motor_home_project/inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Make(models.Model):
@@ -20,14 +21,16 @@ class MotorHome(models.Model):
     id = models.AutoField(primary_key=True)
     vin = models.CharField(max_length=17, unique=True)
     year = models.IntegerField()
+    mileage = models.IntegerField(null=True, blank=True, default=None)
+    condition = models.CharField(max_length=100, null=True, blank=True, default=None)
     model = models.ForeignKey(ModelVehicle, on_delete=models.CASCADE)
     color = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ticket_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=None)
     image = models.ImageField(upload_to='motor_homes/', null=True, blank=True)
     is_new = models.BooleanField(default=True)
     for_sale = models.BooleanField(default=True)
     is_sold = models.BooleanField(default=False)
-    sold_date = models.DateField(null=True, blank=True)
+    sold_date = models.DateField(blank=True, null=True, default=None)
     
     def __str__(self):
         return f"{self.year} {self.model.make.name} {self.model.name} - {self.color}"
--- motor_home_project/sales/models.py
+++ motor_home_project/sales/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from inventory.models import MotorHome
 
 # Create your models here.
+class SalesPerson(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    commission_rate = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Sale(models.Model):
+    id = models.AutoField(primary_key=True)
+    motor_home = models.ForeignKey(MotorHome, on_delete=models.CASCADE)
+    sales_person = models.ForeignKey(SalesPerson, on_delete=models.CASCADE)
+    sale_date = models.DateField()
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    on_hold = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+    warranty_end_date = models.DateField(null=True, blank=True, default=None)
+    
+    def __str__(self):
+        return f"{self.motor_home.year} {self.motor_home.model.name} {self.motor_home.color} - {self.sale_date}"
+    
+    
+
